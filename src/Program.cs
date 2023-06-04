cf41e9db0fa88b2fbdcb0bc8bf763afda73b39dd
Jacob Robert Johnson <75222785+jacobrobertjohnson@users.noreply.github.com>
2023-06-04 10:37:02 -0500

    Denied putting the site in an iframe

--- src/Program.cs
+++ src/Program.cs
@@ -77,6 +77,7 @@ app.Run(async (context) => {
  context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
     context.Response.Headers.Add("Content-Security-Policy", "script-src 'self'");
     context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
+    context.Response.Headers.Add("X-Frame-Options", "DENY");
 });
 
 app.Run();
